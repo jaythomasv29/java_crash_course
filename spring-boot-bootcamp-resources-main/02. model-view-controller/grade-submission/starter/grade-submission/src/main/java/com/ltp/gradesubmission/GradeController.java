67e73efd952729a2004ee295005219eec830bea7
jamesvongampai <jaythomasv29@gmail.com>
2022-10-05 19:00:02 -0700

    add grade-submission project to do POST / GET request and using request params

--- spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,21 +1,92 @@
 package com.ltp.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 // controller annotation
 // entry point for web requests through GET / POST
 @Controller
 public class GradeController {
+    List<Grade> studentGrades = new ArrayList<Grade>(Arrays.asList(
+        new Grade("Harry", "Science", "D+"),
+        new Grade("James", "Sleeping", "C+"),
+        new Grade("Nat", "Coding", "A+"),
+        new Grade("Miley", "Fitness", "B+")
+    ));
+
 
     // handler to map a path for GET requests at a specific endpoint
-    // responds to get requests @ "/hello"
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Im saying hello";
-
+    // responds to get requests @ "/"
+    @GetMapping("/")
+    public String getIndex(Model model) {
+        model.addAttribute("name", "James");
+        return "index";
     }
 
+    // Handler that accepts GET request: "/add"
+    @GetMapping("/add")
+    public String getForm(Model model) {
+        model.addAttribute("grade", new Grade());
+//        System.out.println(new Grade());
+        return "form";
+    }
 
+    // Handler that accepts POST request: "/add"
+    @PostMapping("/addGrade")
+    public String submitForm(Grade grade) {
+        int studentIdx = getStudentGrade(grade.getName());
+
+        if(studentIdx == -1) {
+            studentGrades.add(grade);
+        } else {
+//            Grade existingStudent = studentGrades.get(getStudentGrade(grade.getName()));
+//            existingStudent.setName(grade.getName());
+//            existingStudent.setSubject(grade.getSubject());
+//            existingStudent.setScore(grade.getScore());
+            studentGrades.set(studentIdx, grade);
+
+        }
+        System.out.println(grade);
+
+        return "redirect:/grades";
+    }
+    // handler to map a path for GET requests at a specific endpoint
+    // responds to get requests @ "/grades"
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+
+    @GetMapping("/editStudent")
+    public String editStudent(Model model, @RequestParam(required = false) String name) {
+        int foundIndex = getStudentGrade(name);
+
+//        System.out.println(getStudentGrade(name));
+        if (foundIndex == -1) {
+            model.addAttribute("grade", new Grade());
+        } else {
+            model.addAttribute("grade", studentGrades.get(foundIndex));
+        }
+//        model.addAttribute("grade", foundGrade!= null ? foundGrade : new Grade());
+            return "form";
+    }
+
+    public int getStudentGrade(String name) {
+        for(int i = 0; i < studentGrades.size(); i++) {
+            if(studentGrades.get(i).getName().equals(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
---
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/ShowController.java
@@ -0,0 +1,21 @@
+package com.ltp.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShowController {
+
+    @GetMapping("/shows")
+    public String getShows(Model model) {
+        model.addAttribute("s1", new Show("Breaking Bad", "Ozy", 10.0));
+        model.addAttribute("s2", new Show("Attack on Titan", "Hero", 5.0));
+        model.addAttribute("s3", new Show("Attack on Titan", "Perfect Game", 9.9));
+        model.addAttribute("s4", new Show("Star Wars", "Victory and Death", 5.7));
+        model.addAttribute("s5", new Show("Mr. Robot", "Hack Wars", 6.7));
+        model.addAttribute("expense", 50);
+        model.addAttribute("furniture", "robot");
+        return "shows";
+    }
+}
